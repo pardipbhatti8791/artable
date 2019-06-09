64376ecba02349405d8045619e24e711fe393ead
Pardip Bhatti <pardipbhatti8791@gmail.com>
2019-06-10 00:30:02 +0530

    Initial Commit

---
+++ Artable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Artable
+//
+//  Created by Pardip Bhatti on 10/06/19.
+//  Copyright © 2019 Pardip Bhatti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
